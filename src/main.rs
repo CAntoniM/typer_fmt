14cc4a4ba14e3c36cca99a7291a5de166a74914d
Callum <Callum@cantonim.me>
2023-04-06 13:11:17 +0100

    stubbing out the statement the parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::io::BufRead;
+use std::{io::BufRead, collections::HashMap};
 
-use clap::Parser;
+use clap::{Parser};
 use enigo::{self, KeyboardControllable};
+use regex::{self, Regex};
+
+
 /// A small utility for writing text via a virtual keyboard.
 #[derive(Parser)]
 struct App {
@@ -12,7 +15,34 @@ struct App {
 }
 
 fn parse_statement(statment: &String) -> String {
-    todo!("add string parsing")
+    // Lets make use of rusts regex capture groups to define the pattern we 
+    // want to match and pull out the relavent infomation.
+    let re = Regex::new(r"\?(?P<resolver>.*)\((?P<args>.*)\)").unwrap();
+    let mut return_value = statment.clone();
+
+    let mut replacement_map:HashMap<String,String> = HashMap::new();
+    let mut resolver_map:HashMap<String,fn(String)-> Option<String>> = HashMap::new();
+
+    for capture in re.captures_iter(&statment) {
+
+        let orginal_name = format!("?{}({})",capture.name("resolver").unwrap().as_str(),capture.name("args").unwrap().as_str());
+        
+        if let Some(resolver) = resolver_map.get(capture.name("resolver").unwrap().as_str()) {
+            
+            let replacement_text = match resolver(capture.name("args").unwrap().as_str().to_string().clone()) {  
+                Some(text) => text,
+                None => orginal_name.clone(),
+            };
+
+            replacement_map.insert(orginal_name, replacement_text);
+        }
+        
+
+        for (key,value) in replacement_map.iter() {
+            return_value = return_value.replace(key, value);
+        }
+    }
+    return return_value;
 }
 
 fn main() -> Result<(),String> {
